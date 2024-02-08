86f6e69257029612de7b691e9ce2abb075ef02c8
Vilux <mathias.schneider@adaptechgroup.com>
2024-02-07 19:22:41 -0500

    completion

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -7,14 +7,14 @@ const ExpenseList = () => {
 
     return (
         <table className='table'>
-              <thead className="thead-light">
-            <tr>
-              <th scope="col">Items</th>
-              <th scope="col">Quantity</th>
-              <th scope="col">Unit Price</th>
-              <th scope="col">Items Price</th>
-              <th scope="col">Remove</th>
-            </tr>
+            <thead className="thead-light">
+                <tr>
+                <th scope="col">Items</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Unit Price</th>
+                <th scope="col">Items Price</th>
+                <th scope="col">Remove</th>
+                </tr>
           </thead>
             <tbody>
             {expenses.map((expense) => (
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -20,20 +20,20 @@ export const AppReducer = (state, action) => {
                 ...state,
             };
 
-            case 'RED_QUANTITY':
-                state.expenses.map((expense)=>{
-                    if(expense.name === action.payload.name) {
-                        expense.quantity = expense.quantity - action.payload.quantity;
-                    }
-                    expense.quantity = expense.quantity < 0 ? 0: expense.quantity;
-                    new_expenses.push(expense);
-                    return true;
-                })
-                state.expenses = new_expenses;
-                action.type = "DONE";
-                return {
-                    ...state,
-                };
+        case 'RED_QUANTITY':
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name) {
+                    expense.quantity = expense.quantity - action.payload.quantity;
+                }
+                expense.quantity = expense.quantity < 0 ? 0: expense.quantity;
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expenses = new_expenses;
+            action.type = "DONE";
+            return {
+                ...state,
+            };
         case 'DELETE_ITEM':
             state.expenses.map((expense)=>{
                 if(expense.name === action.payload.name) {
@@ -47,7 +47,7 @@ export const AppReducer = (state, action) => {
             return {
                 ...state,
             };
-    case 'CHG_LOCATION':
+        case 'CHG_LOCATION':
             action.type = "DONE";
             state.Location = action.payload;
             return {
@@ -56,7 +56,7 @@ export const AppReducer = (state, action) => {
 
         default:
             return state;
-    }
+        }
 };
 
 // 1. Sets the initial state when the app loads
